d9e750e276572269d423f1fa6051864e21a8d3e6
fmontesclaros <fmontesclaros@fullscale.io>
2022-06-07 11:28:29 +0800

    Update DashboardContact.js

--- src/components/views/Dashboard/DashboardContact.js
+++ src/components/views/Dashboard/DashboardContact.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Typography from '../../common/Typography';
+import TextField from '../../common/TextField'; 
 
 function DashboardContact() {
   return (
@@ -26,12 +27,43 @@ function DashboardContact() {
       <Typography variant="subtitle1" sx={{ my: 3 }}>
         We are here to help. Get in touch!
       </Typography>
+      <Typography variant="h4" component="h4" align="center" gutterBottom>
+          Contact Us
+      </Typography>
       <Box
         component="img"
         src="/images/producBuoy.svg"
         alt="buoy"
         sx={{ width: 60 }}
       />
+      <Box component="form" sx={{ maxWidth: 400 }}>
+        <TextField
+          noBorder
+          placeholder="Your name"
+          variant="outlined"
+          sx={{ width: '100%', mt: 3, mb: 2 }}
+        />
+        <TextField
+          noBorder
+          placeholder="Your email"
+          variant="outlined"
+          sx={{ width: '100%', mt: 1, mb: 2 }}
+        />
+        <TextField
+          noBorder
+          placeholder="Your message"
+          variant="outlined"
+          sx={{ width: '100%', mt: 1, mb: 4 }}
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          sx={{ width: '100%' }}
+        >
+          Submit
+        </Button>
+      </Box>
     </Container>
   );
 }
