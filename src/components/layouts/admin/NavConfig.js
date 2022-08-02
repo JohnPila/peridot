4235f45539a0f90f4be7993a672017a04a61dc18
fmontesclaros <fmontesclaros@fullscale.io>
2022-08-03 03:26:28 +0800

    Removed airport transfer menu in admin

--- src/components/layouts/admin/NavConfig.js
+++ src/components/layouts/admin/NavConfig.js
@@ -4,7 +4,6 @@ import Iconify from '../../common/Iconify';
 // ----------------------------------------------------------------------
 
 const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
-const getLargeIcon = (name) => <Iconify icon={name} width={35} height={35} />;
 
 const navConfig = [
   {
@@ -23,25 +22,10 @@ const navConfig = [
     icon: getIcon('eva:shopping-bag-fill'),
   },
   {
-    title: 'Airport Transfer',
-    path: '/admin/all-airport-bookings',
-    icon: getLargeIcon('ic:round-connecting-airports'),
-  },
-  {
     title: 'Car Rentals',
     path: '/admin/car-rentals',
     icon: getIcon('bxs:car'),
   },
-  // {
-  //   title: 'register',
-  //   path: '/register',
-  //   icon: getIcon('eva:person-add-fill'),
-  // },
-  // {
-  //   title: 'Not found',
-  //   path: '/404',
-  //   icon: getIcon('eva:alert-triangle-fill'),
-  // },
 ];
 
 export default navConfig;
